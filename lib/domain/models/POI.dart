174a966f3ea60b021d9041de3990306f06354f28
Loruski <lorusso116@gmail.com>
2025-11-07 00:20:04 +0100

    cities and pois json samples and corrections to model

--- lib/domain/models/POI.dart
+++ lib/domain/models/POI.dart
@@ -1,11 +1,15 @@
+import 'package:re_discover/domain/models/Position.dart';
 import 'package:re_discover/domain/models/Quiz.dart';
 
 
 class POI{
+  final int id;
   String name;
+  String description;
   List<String> images;
   List<Quiz> quizzes;
   String qrCode;
+  Position position;
 
-  POI(this.name, this.images, this.quizzes, this.qrCode);
+  POI(this.id, this.name, this.description, this.images, this.quizzes, this.qrCode, this.position);
 }
--- lib/domain/models/hint.dart
+++ lib/domain/models/hint.dart
@@ -1,6 +1,7 @@
 class Hint {
+  int id;
   String description;
   List<String> images;
 
-  Hint(this.description, this.images);
+  Hint(this.id, this.description, this.images);
 }
