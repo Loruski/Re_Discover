4552542919620fe6eca19a4c813011f723bf993c
Giacomix02 <paoloccigiacomo@gmail.com>
2025-11-09 18:44:46 +0100

    added margin to button to center map to user position

--- lib/ui/MAP/widgets/OSM_custom.dart
+++ lib/ui/MAP/widgets/OSM_custom.dart
@@ -18,45 +18,48 @@ class _OsmCustom extends State<OsmCustom> {
   @override
   Widget build(BuildContext context) {
     return Expanded(
-        child: FlutterMap(
-          mapController: widget.mapViewModel.mapController,
-            options: MapOptions(
-              initialCenter: widget.mapViewModel.currentCenter ?? const LatLng(42.405916, 12.856193),
-              initialZoom: widget.mapViewModel.currentZoom ?? 13.0,
-              onPositionChanged: (position, hasGesture) {
-                widget.mapViewModel.updateMapPosition(position.center!, position.zoom!);
-              },
-            ),
-          children: [
-            TileLayer(
-              urlTemplate:
-              'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
-              userAgentPackageName: 'it.univaq.egs.re_discover',
-            ),
-            CurrentLocationLayer(
-              alignPositionOnUpdate: AlignOnUpdate.always,
-              alignDirectionOnUpdate: AlignOnUpdate.never,
-              style: LocationMarkerStyle(
-                marker: const DefaultLocationMarker(
-                  child: Icon(
-                    Icons.my_location,
-                    color: Colors.blue,
-                    size: 3,
-                  ),
-                ),
-                markerSize: const Size(20, 20),
-                markerDirection: MarkerDirection.heading,
+      child: FlutterMap(
+        mapController: widget.mapViewModel.mapController,
+        options: MapOptions(
+          initialCenter:
+              widget.mapViewModel.currentCenter ??
+              const LatLng(42.405916, 12.856193),
+          initialZoom: widget.mapViewModel.currentZoom ?? 13.0,
+          onPositionChanged: (position, hasGesture) {
+            widget.mapViewModel.updateMapPosition(
+              position.center!,
+              position.zoom!,
+            );
+          },
+        ),
+        children: [
+          TileLayer(
+            urlTemplate: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
+            userAgentPackageName: 'it.univaq.egs.re_discover',
+          ),
+          CurrentLocationLayer(
+            alignPositionOnUpdate: AlignOnUpdate.always,
+            alignDirectionOnUpdate: AlignOnUpdate.never,
+            style: LocationMarkerStyle(
+              marker: const DefaultLocationMarker(
+                child: Icon(Icons.my_location, color: Colors.blue, size: 3),
               ),
+              markerSize: const Size(20, 20),
+              markerDirection: MarkerDirection.heading,
             ),
-            Align(
-              alignment: Alignment.bottomRight,
+          ),
+          Align(
+            alignment: Alignment.bottomRight,
+            child: Container(
+              margin: const EdgeInsets.only(right: 20, bottom: 20),
               child: FloatingActionButton(
-                  onPressed: widget.mapViewModel.getUserPosition,
-                  child: const Icon(Icons.my_location)
+                onPressed: widget.mapViewModel.getUserPosition,
+                child: const Icon(Icons.my_location),
               ),
-            )
-          ],
-        )
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
