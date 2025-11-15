333ddd052d5092d988032fc35bcb0f75cbe37ad3
mshaa1 <dicrescemanuel@gmail.com>
2025-11-15 12:40:50 +0100

    Home widgets' refactoring

--- lib/ui/HOME/screens/home_screen.dart
+++ lib/ui/HOME/screens/home_screen.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'package:re_discover/ui/HOME/screens/city_selector_screen.dart';
 import 'package:re_discover/ui/HOME/view_model/home_view_model.dart';
 import 'package:re_discover/ui/HOME/widgets/home_screen_answers_accuracy_cards.dart';
 import 'package:re_discover/ui/HOME/widgets/home_screen_exploration_button.dart';
@@ -25,50 +24,27 @@ class HomeScreenContent extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-
     return Scaffold(
       body: SafeArea(
-        child: Center(
-          child: SingleChildScrollView(
-            child: Padding(
-              padding: const EdgeInsets.symmetric(vertical: 20.0),
+          child: Padding(
+            padding: const EdgeInsets.all(20),
+            child: Center(
               child: Column(
+                spacing: 20,
                 mainAxisAlignment: MainAxisAlignment.center,
                 crossAxisAlignment: CrossAxisAlignment.center,
                 children: [
                   HomeScreenHeader(),
-
-                  SizedBox(height: 30),
-
-                  Column(
-                    children: [
-                      HomeScreenLevelCard(),
-                      HomeScreenAnswersAccuracyCards(),
-                      HomeScreenExplorationButton(),
-
-                      SizedBox(height: 5),
-
-                      TextButton(
-                        onPressed: () {
-                          Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                  builder: (context) => const CitySelectorScreen()
-                              )
-                          );
-                        },
-                        child: const Text("Cambia città"),
-                      ),
-
-                    ],
-                  ),
-                ],
-              ),
+                  HomeScreenLevelCard(),
+                  HomeScreenAnswersAccuracyCards(),
+                  HomeScreenExplorationButton(),
+                ]
+              )
             ),
-          ),
-        ),
+          )
       ),
-      floatingActionButton: FloatingActionButton(onPressed: () => Navigator.push(context, MaterialPageRoute(builder: (context) => QuizScreen()))),
+        floatingActionButton: FloatingActionButton(onPressed: () => Navigator.push(context, MaterialPageRoute(builder: (context) => QuizScreen()))),
     );
   }
+
 }
--- lib/ui/HOME/widgets/home_screen_exploration_button.dart
+++ lib/ui/HOME/widgets/home_screen_exploration_button.dart
@@ -1,35 +1,40 @@
 import 'package:flutter/material.dart';
+import 'package:re_discover/ui/HOME/screens/city_selector_screen.dart';
 
 class HomeScreenExplorationButton extends StatelessWidget {
   const HomeScreenExplorationButton({super.key});
 
-
   @override
   Widget build(BuildContext context) {
-    return ElevatedButton.icon(
-      onPressed: () {
-        // TODO: Implement navigation
-      },
-      style: ElevatedButton.styleFrom(
-        padding: const EdgeInsets.symmetric(
-          horizontal: 30,
-          vertical: 15,
+    return Column(
+      children: [
+        ElevatedButton.icon(
+          onPressed: () {
+            // TODO: Implement navigation
+          },
+          style: ElevatedButton.styleFrom(
+            padding: const EdgeInsets.symmetric(horizontal: 30, vertical: 15),
+            shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(24)),
+            backgroundColor: Colors.purple,
+          ),
+          icon: const Icon(Icons.explore_outlined, color: Colors.white),
+          label: const Text(
+            "Inizia esplorazione",
+            style: TextStyle(color: Colors.white),
+          ),
         ),
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(24),
-        ),
-        backgroundColor: Colors.purple,
-      ),
-      icon: const Icon(
-        Icons.explore_outlined,
-        color: Colors.white,
-      ),
-      label: const Text(
-        "Inizia esplorazione",
-        style: TextStyle(color: Colors.white),
-      ),
+        TextButton(
+          onPressed: () {
+            Navigator.push(
+                context,
+                MaterialPageRoute(
+                    builder: (context) => const CitySelectorScreen()
+                )
+            );
+          },
+          child: const Text("Cambia città"),
+        )
+      ],
     );
   }
-
-
 }
