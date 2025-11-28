4e2d2bec001db2d441ad5003290bdc66d5ef25ba
mshaa1 <dicrescemanuel@gmail.com>
2025-11-28 16:16:06 +0100

    Currently logged in User is not retrieved via MainViewModel

--- lib/ui/HOME/view_model/home_view_model.dart
+++ lib/ui/HOME/view_model/home_view_model.dart
@@ -1,20 +1,6 @@
 import 'package:flutter/foundation.dart';
 
+
 class HomeViewModel extends ChangeNotifier {
-  // insert repository here
-
-  int counter = 0;
-
-
-  Future<void> _load() async {
-    // load at the first time
-    notifyListeners();
-  }
-
-  void incrementCounter() {
-    counter++;
-    notifyListeners();
-  }
-
 
 }
